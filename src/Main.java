f45c4587ec3602cf0e4c540523cd2c7685b2153a
Michal Urbanczyk <michaurbanczyk@gmail.com>
2025-09-23 15:14:45 +0200

    calcEngine project

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        double value1 = 100.0d;
+        double value2 = 50.d;
+        double result = 0.0d;
+        char operationCode = 'd';
+
+        if (operationCode == 'a')
+            result = value1 + value2;
+        else if (operationCode == 's')
+            result = value1 - value2;
+        else if (operationCode == 'm')
+            result = value1 * value2;
+        else if (operationCode == 'd')
+            result = value1 / value2;
+        else
+            result = 0.0d;
+
+        System.out.println(result);
     }
 }
