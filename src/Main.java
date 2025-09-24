7b4c31e37095e922ad34f7408197451e9abe0ab6
Michal Urbanczyk <michaurbanczyk@gmail.com>
2025-09-24 19:36:49 +0200

    calcEngine changed to for loop and switch statement

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,32 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        double value1 = 100.0d;
-        double value2 = 50.d;
-        double result = 0.0d;
-        char operationCode = 'd';
+        double[] leftVals = {100.0d, 25.0d, 225.0d, 11.0d};
+        double[] rightVals = {50.0d, 92.0d, 17.0d, 3.0d};
+        char[] operationCode = {'d', 'a', 's', 'm'};
+        double[] results = new double[operationCode.length];
 
-        if (operationCode == 'a')
-            result = value1 + value2;
-        else if (operationCode == 's')
-            result = value1 - value2;
-        else if (operationCode == 'm')
-            result = value1 * value2;
-        else if (operationCode == 'd')
-            result = value1 / value2;
-        else
-            result = 0.0d;
-
-        System.out.println(result);
+        for (int i = 0; i < operationCode.length; i++) {
+            switch (operationCode[i]) {
+                case 'a':
+                    results[i] = leftVals[i] + rightVals[i];
+                    break;
+                case 's':
+                    results[i] = leftVals[i] - rightVals[i];
+                    break;
+                case 'm':
+                    results[i] = leftVals[i] * rightVals[i];
+                    break;
+                case 'd':
+                    results[i] = rightVals[i] != 0 ? leftVals[i] / rightVals[i] : 0.0d;
+                    break;
+                default:
+                    System.out.println("Invalid opCode:" + operationCode[i]);
+                    results[i] = 0.0d;
+                    break;
+            }
+        }
+        System.out.println("Results:" + Arrays.toString(results));
     }
 }
