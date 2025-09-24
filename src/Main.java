4c13a3cf6e2fe57048b65e0baf1bf3b1f595e464
Michal Urbanczyk <michaurbanczyk@gmail.com>
2025-09-24 20:00:50 +0200

    args to the main function

--- src/Main.java
+++ src/Main.java
@@ -7,27 +7,41 @@ public class Main {
         char[] operationCode = {'d', 'a', 's', 'm'};
         double[] results = new double[operationCode.length];
 
-        for (int i = 0; i < operationCode.length; i++) {
-            switch (operationCode[i]) {
-                case 'a':
-                    results[i] = leftVals[i] + rightVals[i];
-                    break;
-                case 's':
-                    results[i] = leftVals[i] - rightVals[i];
-                    break;
-                case 'm':
-                    results[i] = leftVals[i] * rightVals[i];
-                    break;
-                case 'd':
-                    results[i] = rightVals[i] != 0 ? leftVals[i] / rightVals[i] : 0.0d;
-                    break;
-                default:
-                    System.out.println("Invalid opCode:" + operationCode[i]);
-                    results[i] = 0.0d;
-                    break;
-            }
+        if(args.length == 0){
+            for (int i = 0; i < operationCode.length; i++) {
+            results[i] = execute(operationCode[i], leftVals[i], rightVals[i]);
         }
-        for (double result : results)
-            System.out.println("Result: " + result);
+            for (double result : results)
+                System.out.println("Result: " + result);
+        } else if (args.length == 3){
+            handleCommandLine(args);
+        }
+        else {
+            System.out.println("Wrong number of arguments");
+        }
+    }
+
+    static double execute(char operationCode, double leftVal, double rightVal) {
+        switch (operationCode) {
+            case 'd':
+                return leftVal + rightVal;
+            case 'a':
+                return leftVal - rightVal;
+            case 's':
+                return leftVal * rightVal;
+            case 'm':
+                return rightVal != 0.0d ? leftVal / rightVal : 0.0d;
+            default:
+                System.out.println("Invalid operationCode:" + operationCode);
+                return 0.0d;
+        }
+    }
+
+    static void handleCommandLine(String[] args) {
+        char opCode = args[0].charAt(0);
+        double leftVal = Double.parseDouble(args[1]);
+        double rightVal = Double.parseDouble(args[2]);
+        double result =  execute(opCode, leftVal, rightVal);
+        System.out.println("Result: " + result);
     }
 }
