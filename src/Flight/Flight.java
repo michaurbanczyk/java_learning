6a93ef69e434021c1b018757d6986e9e4d64765f
Michal Urbanczyk <michaurbanczyk@gmail.com>
2025-09-25 10:10:06 +0200

    working with java classes

--- src/Flight/Flight.java
+++ src/Flight/Flight.java
@@ -1,2 +1,19 @@
-package Flight;public class Flight {
+package Flight;
+
+public class Flight {
+    private int passengers, seats = 150;
+    private static int allPassengers;
+    String name;
+
+    Flight(String name){
+        this.name = name;
+    }
+
+    public void addPassenger(){
+        allPassengers++;
+    }
+
+    public static int getAllPassengers(){
+        return allPassengers;
+    }
 }
--- src/Flight/Main.java
+++ src/Flight/Main.java
@@ -1,2 +1,12 @@
-package Flight;public class Main {
+package Flight;
+import static Flight.Flight.getAllPassengers;
+
+public class Main {
+    public static void main(String[] args) {
+        Flight f1 = new Flight("ABC");
+        Flight f2 = new Flight("ABC");
+        f1.addPassenger();
+        f2.addPassenger();
+        System.out.println(getAllPassengers());
+    }
 }
